601a274e3052619c31a97abb072c75ec1c9c80c5
Overderim <customerserv@overderim.com>
2024-10-29 13:51:24 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('quoteForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    document.getElementById('confirmationMessage').classList.remove('hidden');
+});
