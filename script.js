921053c86cd3672ecd20aea529bdce2d205f488c
Overderim <customerserv@overderim.com>
2024-10-29 14:07:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ document.getElementById('quoteForm').addEventListener('submit', function(e) {
     e.preventDefault();
     document.getElementById('confirmationMessage').classList.remove('hidden');
 });
+
